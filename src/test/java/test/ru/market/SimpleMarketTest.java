4294522a0c2543f13132cf6d45cf8a4951866307
Sergey Yakovlev <web-wind@ya.ru>
2020-05-16 19:43:36 +0300

    some change

--- src/test/java/test/ru/market/SimpleMarketTest.java
+++ src/test/java/test/ru/market/SimpleMarketTest.java
@@ -5,13 +5,11 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import pages.market.MarketPage;
-import pages.market.MobilPhonePage;
-import test.BaseTest;
-
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Домашнее задание
@@ -32,35 +30,27 @@ import java.util.List;
  * -- Проверить, что в списке характеристик появилась позиция "Операционная система"
  * - Нажать на опцию "различающиеся характеристики"
  * -- Проверить, что позиция "Операционная система" не отображается в списке характеристик
- * <p>
- * Критерии оценки: +1 балл - код компилируется и запускается
- * +1 балл - код запускается без дополнительных действий со стороны проверяющего (не нужно скачивать WebDriver, решать конфликты зависимостей и т.п.)
- * +1 балл - задействован WebDriverManager
- * +1 балл - логи пишутся в консоль и файл
- * +1 балл - тест проходит без падений (допускается падение теса только при некорректной работе SUT)
- * +2 балла - тест проходит при разной скорости интернет-соединения
- * +1 балл - используются явные и неявные ожидания
- * <p>
- * -1 балл - за каждый Thread.sleep()
  */
 public class SimpleMarketTest {
     WebDriver driver;
     WebDriverWait wait;
-    WebElement all;
 
     @BeforeTest
     public void upDriver() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        wait = new WebDriverWait(driver, 5);
+        wait = new WebDriverWait(driver, 10);
     }
 
     @Test(description = "Test YandexMarket page and filters on market")
     public void marketTest() {
         driver.navigate().to("http://market.yandex.ru");
         try {
-            WebElement pp = driver.findElement(By.xpath("//div[@class=\"popup2__content\"]"));
+            WebElement pp = wait.until(driver -> driver.findElement(By.cssSelector("div.popup2__content")));
             while (pp.isDisplayed() && pp.isEnabled()) {
                 if (!(pp.isEnabled() && pp.isDisplayed())) {
                     break;
@@ -68,37 +58,77 @@ public class SimpleMarketTest {
             }
         } catch (NoSuchElementException e) {
         }
-
-        wait.until(driver -> driver.findElement(By.xpath("//div[@class = \"_35SYuInI1T _1vnugfYUli\"]/a[@href = \"/catalog--elektronika/54440\"]/.."))).click();
-        wait.until(driver -> driver.findElement(By.xpath("//a[@href=\"/catalog--mobilnye-telefony/54726/list?hid=91491\"]"))).click();
-        driver.findElement(By.xpath("//input[@id = \"7893318_7701962\"]/..")).click(); //Сяоми
-        driver.findElement(By.xpath("//input[@id=\"7893318_1007740\"]/..")).click(); //ZTE
-        driver.findElement(By.xpath("(//a[@class=\"link link_theme_major n-filter-sorter__link i-bem link_js_inited\"])[2]")).click();
-
+        wait.until(driver -> driver.findElement(By
+                .xpath("//div[@class = \"_35SYuInI1T _1vnugfYUli\"]/a[@href = \"/catalog--elektronika/54440\"]/.."))).click();
+        wait.until(driver -> driver.findElement(By
+                .xpath("//a[@href=\"/catalog--mobilnye-telefony/54726/list?hid=91491\"]"))).click();
+        wait.until(driver -> driver.findElement(By
+                .xpath("//input[@id = \"7893318_7701962\"]/.."))).click();  //  Сяоми
+        wait.until(driver -> driver.findElement(By
+                .xpath("//input[@id=\"7893318_1007740\"]/.."))).click();  // ЗТЕ
+        wait.until(driver -> driver.findElement(By
+                .xpath("//a[text()=\"по цене\"]"))).click();
         while (true) {
             try {
                 wait.until(ExpectedConditions
-                        .visibilityOfElementLocated(By.xpath("//a[contains(@class,'button button_size_m')]"))).click();
+                        .visibilityOfElementLocated(By
+                                .xpath("//a[@role='button' and contains(@class,'button button_size_m')]"))).click();
             } catch (TimeoutException e) {
                 break;
-            }
+            }catch (StaleElementReferenceException e){}
         }
-        List<WebElement> list = driver.findElements(By.xpath("//div[@class='n-snippet-cell2__brand-name']"));
-        System.out.println(list.size());
+
+        List<WebElement> list2 = driver.findElements(By.xpath("//div[contains(@data-bem,\"n-user-lists_type_compare\")]/."));
+        System.out.println(list2.size());
+
         int countXiaomi = 0;
         int countZTE = 0;
 
-        for (int i = 0; i < list.size() ; i++) {
-            System.out.println(list.get(i).getText());
-            if(list.get(i).getText().equals("XIAOMI")
-            && countXiaomi == 0){
+        for (int i = 0; i < list2.size(); i++) {
+            System.out.println(getNameSmartphone(list2.get(i).getAttribute("data-bem")));
+            if (countXiaomi == 1 && countZTE == 1) {
+                break;
+            } else if (getNameSmartphone(list2.get(i).getAttribute("data-bem")).contains("Xiaomi")
+                    && countXiaomi == 0) {
+                list2.get(i).click();
+                Assert.assertTrue(driver.findElement(
+                        By.xpath("//div[contains(text(), \"добавлен к сравнению\") and contains(text(), \"Xiaomi\")]")).isDisplayed());
                 System.out.println("добавили Сяоми");
                 countXiaomi++;
-            }else if(list.get(i).getText().equals("ZTE")
-            && countZTE == 0){
+            } else if (getNameSmartphone(list2.get(i).getAttribute("data-bem")).contains("ZTE")
+                    && countZTE == 0) {
+                list2.get(i).click();
+                Assert.assertTrue(driver.findElement(
+                        By.xpath("//div[contains(text(), \"добавлен к сравнению\") and contains(text(), \"ZTE\")]")).isDisplayed());
                 System.out.println("Добавили ЗТЕ");
                 countZTE++;
             }
         }
+        wait.until(driver -> driver.findElement(
+                By.cssSelector("a[class^=button][href$=rmmbr]"))).click();
+        int countInComparisson = driver.findElements(By.cssSelector("div[class^=n-compare-cell][data-bem]")).size();
+        Assert.assertEquals(2, countInComparisson);  //  Ассерт количества проверяемых на странице
+        wait.until(driver -> driver.findElement(
+                By.cssSelector("span[class = \"link n-compare-show-controls__all\"][role=button] .link__inner"))).click();
+         try {
+             Assert.assertTrue(wait.until(ExpectedConditions
+                     .visibilityOf(driver.findElement(By.xpath("//div[text()=\"Операционная система\"]")))).isDisplayed());
+         }catch (TimeoutException e){}
+        wait.until(driver -> driver.findElement(
+                By.cssSelector("span[class = \"link n-compare-show-controls__diff\"][role=button] .link__inner"))).click();
+        try {
+            Assert.assertFalse(wait.until(ExpectedConditions
+                    .visibilityOf(driver.findElement(By.xpath("//div[text()=\"Операционная система\"]")))).isDisplayed());
+        }catch (TimeoutException e){}
+    }
+
+    public String getNameSmartphone( String json ) {
+        String tempList[] = json.split(":");
+        return getClearName(tempList[3]);
+    }
+
+    public String getClearName( String fullName ) {
+        String tempList[] = fullName.split("\"");
+        return tempList[1];
     }
 }
