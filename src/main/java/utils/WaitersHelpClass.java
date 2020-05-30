0ea6ee77aca51473afaae5ecd4b7d757bcccf4f4
Sergey Yakovlev <web-wind@ya.ru>
2020-05-30 10:58:10 +0300

    some change

--- src/main/java/utils/WaitersHelpClass.java
+++ src/main/java/utils/WaitersHelpClass.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public abstract class WaitersHelpClass {
+public class WaitersHelpClass {
 
     public String getNameSmartphone( String json ) {
         return getClearName(json.split(":")[3]);
